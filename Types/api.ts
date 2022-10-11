644be01cf5ae4af9b1cb535a0a5de9e135ac3d04
pumbas600 <joshjeffers600@gmail.com>
2022-10-11 17:07:08 +1300

    feat: Add remaining user endpoints and create UserIdParam type

--- Types/api.ts
+++ Types/api.ts
@@ -1,16 +1,27 @@
-import { LoginRequest } from "./requests/UserRequests"
-import { LoginData, GetSelfData, GetAllUsersData, GetUserData } from "./responses/UserResponsesData"
+import { UserIdParam } from './params';
+import { GiveRolesRequest, LoginRequest, RemoveRolesRequest, UpdateUserRequest } from './requests/UserRequests';
+import {
+    LoginData,
+    GetSelfData,
+    GetAllUsersData,
+    GetUserData,
+    UpdateUserData,
+    GiveRolesData,
+    RemoveRolesData,
+} from './responses/UserResponsesData';
 
-
-export interface Endpoint<Req, Res, Params extends Record<string, string> = undefined> {
-    req: Req,
-    res: Res,
-    params: Params,
+export interface Endpoint<Req, Res, Params = undefined> {
+    req: Req;
+    res: Res;
+    params: Params;
 }
 
 export default interface API {
-    'POST /api/users/login': Endpoint<LoginRequest, LoginData>;
     'GET /api/users/@me': Endpoint<undefined, GetSelfData>;
     'GET /api/users/': Endpoint<undefined, GetAllUsersData>;
-    'GET /api/users/:userId': Endpoint<undefined, GetUserData, { userId: string }>;
+    'GET /api/users/:userId': Endpoint<undefined, GetUserData, UserIdParam>;
+    'POST /api/users/login': Endpoint<LoginRequest, LoginData>;
+    'POST /api/users/:userId/roles/': Endpoint<GiveRolesRequest, GiveRolesData, UserIdParam>;
+    'PATCH /api/users/:userId': Endpoint<UpdateUserRequest, UpdateUserData, UserIdParam>;
+    'DELETE /api/users/:userId/roles': Endpoint<RemoveRolesRequest, RemoveRolesData, UserIdParam>;
 }
