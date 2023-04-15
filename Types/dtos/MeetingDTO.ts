678be2b3a5881bd1c6c246385655983e672bcb42
tlooi <t.looi@outlook.com>
2023-04-15 21:38:44 +1200

    fix: restore old DTO types

--- Types/dtos/MeetingDTO.ts
+++ Types/dtos/MeetingDTO.ts
@@ -15,8 +15,8 @@ export default interface MeetingDTO {
     /** The time value in ms when this meeting is scheduled for. */
     time: number;
     location: string;
-    attendance: Map<string, AttendanceDTO>;
+    attendance: AttendanceDTO;
 
-    /** The description for this meeting or null if there isn't one. */
-    description: string | null;
+    /** The optional description for this meeting. */
+    description?: string;
 }
